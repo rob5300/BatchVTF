02604e29f1a135920adcf7cd4db5e19a508e88e6
ROBERTS_PC <rob5300@hotmail.com>
2023-10-05 23:40:46 +0100

    perf: start making vtex script

---
+++ batchvtex.py
@@ -0,0 +1,53 @@
+import os
+from os import path
+import glob
+import sys
+
+here = os.path.dirname(__file__)
+
+supported_formats = ["psd", "tga"]
+
+def GetGameInfoPath(dir):
+    return path.join(dir, "game", "tf", "gameinfo.txt")
+
+def ProcessFiles():
+    gameinfo = GetGameInfoPath(here)
+    while not path.exists(gameinfo):
+        print("Enter path of gameinfo:")
+        gameinfo = input()
+
+    content = path.normpath(path.join(gameinfo, "..", "..", "..", "content"))
+    game = path.normpath(path.join(gameinfo, "..", ".."))
+    vtex = path.join(game, "bin", "vtex.exe")
+
+    to_compile = []
+    for ext in supported_formats:
+        to_compile.extend(glob.glob("./**/*." + ext, root_dir=content, recursive=True))
+
+    print(f"Found {len(to_compile)} files")
+
+    
+    
+def ProcessFile(vtex: str, gameinfo: str, content: str, game: str, file: str):
+    vtexcmd = f"{vtex} -game {gameinfo} -outdir {content} -nopause {file}"
+
+    print("> " + vtexcmd)
+    result = os.system(vtexcmd)
+    if os.name != "nt":
+        result = os.waitstatus_to_exitcode(result)
+    
+    if result != 0:
+        raise Exception(f"Failed when executing VTFCmd, Code: {result}")
+    else:
+        print(f"Success!")
+
+if len(sys.argv) > 1:
+    here = sys.argv[1]
+    if not path.exists(here):
+        here = path.join(__path__, sys.argv[1])
+
+if not path.exists(here):
+    print(f"Given dir '{here}' does not exist")
+    exit(1)
+
+ProcessFiles()
